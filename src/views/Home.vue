9037e95b880bd2425ef62c487c1f4033a8ad4778
Alex <lex.courtney18@gmail.com>
2023-02-19 15:10:35 -0600

    remove boiler plate content

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta, asperiores?</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta, asperiores?</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {}
+}
+</script>
